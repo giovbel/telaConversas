6b3aede1d8abfe84d91b9a00434536ce2e342457
GIOVANNA SILVA BELO <23111786@dsn127m1242315.cfp127aluno.local>
2024-10-10 09:38:06 -0300

    pronto

---
+++ app/src/main/java/br/senai/sp/jandira/teladasnotas/MainActivity.kt
@@ -0,0 +1,314 @@
+package br.senai.sp.jandira.teladasnotas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.teladasnotas.ui.theme.TelaDasNotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TelaDasNotasTheme {
+                    Greeting(
+
+                    )
+                }
+            }
+        }
+    }
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .height(70.dp)
+                .fillMaxWidth()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF213787),
+                            Color(0xFF245FB0),
+                            Color(0xFF6E96E8)
+                        )
+                    ),
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(60.dp)
+                )
+                Text(
+                    text = "CHAT",
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium
+                )
+                Card(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .border(4.dp, Color(0xFF9DBFEF), RoundedCornerShape(30.dp)),
+                    shape = RoundedCornerShape(100.dp),
+                ) {
+
+                }
+            }
+
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(630.dp)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFFEDF5FF),
+                            Color(0xFFEDF5FF),
+                            Color(0xFFA5D3FF),
+                        )
+                    ),
+                ),
+            shape = RoundedCornerShape(0.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+        ) {
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(75.dp),
+                verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Text(
+                        text = "Conversas",
+                        fontSize = 18.sp,
+                        color = Color(0xFF215CB9),
+                        fontWeight = FontWeight.Medium
+
+                    )
+
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.macallmenotelefone),
+                    contentDescription = "",
+                    modifier = Modifier.size(70.dp)
+                )
+
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Text(
+                        text = "Psicólogos",
+                        fontSize = 18.sp,
+                        color = Color(0xFF215CB9),
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+
+
+            }
+            HorizontalDivider(color = Color(0xFF3758C3), thickness = 2.dp)
+
+            Row (
+                modifier = Modifier.height(90.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Card (
+                    modifier = Modifier
+                        .height(80.dp)
+                        .width(80.dp)
+                        .border(width = 2.dp, color = Color.Black, shape = CircleShape),
+                        RoundedCornerShape(100.dp)
+                ){
+
+                }
+                Column {
+                    Text(
+                        text = "Luana Santos",
+                        color = Color(0xFF215CB9),
+                        fontWeight = FontWeight.Bold
+
+
+                    )
+                    Text(text = "oi")
+
+                }
+
+            }
+
+            HorizontalDivider(color = Color(0xFF3758C3), thickness = 2.dp)
+        }
+        Box(
+            modifier = Modifier
+                .height(70.dp)
+                .fillMaxWidth()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF213787),
+                            Color(0xFF245FB0),
+                            Color(0xFF6E96E8)
+                        )
+                    ),
+                )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.calendarioicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.chaticon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.diarioicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.autoajuda),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.doacaoicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TelaDasNotasTheme {
+        Greeting()
+    }
+}
